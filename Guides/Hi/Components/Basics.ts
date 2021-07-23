91c3b2a9dc07238ab5e55fc2e86d13b9f4924a37
Viraj Shah <shahvirajd@gmail.com>
2021-07-23 05:40:26 -0400

    Added basic features to radio buttons

--- Guides/Hi/Components/Basics.ts
+++ Guides/Hi/Components/Basics.ts
@@ -88,6 +88,34 @@ export class RadioButton extends View {
     constructor() {
         super('ion-icon');
         this.body.setAttribute('name', 'radio-button-off');
+        this.body.addEventListener('click', ev => {
+            this.state.selected = !this.state.selected;
+        });
+    }
+
+    setSelected(value: boolean): this {
+        this.state.selected = value;
+        return this;
+    }
+
+    isSelected(): boolean {
+        return this.state.selected;
+    }
+
+    toggle(): this {
+        this.state.selected = !this.state.selected;
+        return this;
+    }
+
+    whenClicked(callback: (ev: HumanEvent) => void): this {
+        this.body.addEventListener('click', (browserEvent: Event) => {
+            callback({
+                type: 'Click',
+                view: this,
+                browserEvent,
+            });
+        });
+        return this;
     }
 }
 
