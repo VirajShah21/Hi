1925caa58ac42728fa7cf340dd500219866e52fb
Viraj Shah <shahvirajd@gmail.com>
2021-08-24 14:25:09 -0400

    Added adaptability

--- Client/ts/Components/BlockCode.ts
+++ Client/ts/Components/BlockCode.ts
@@ -1,21 +1,30 @@
+import { StateObject } from '@Hi/Types/states';
 import View from '@Hi/View';
 
 export default class BlockCode extends View {
     public override body: HTMLPreElement;
+    public state = StateObject(
+        {
+            code: '',
+        },
+        () => {
+            this.body.innerText = this.state.code;
+        }
+    );
 
     constructor(text: string) {
         super('pre');
-        this.body.innerText = text;
+        this.state.code = text;
         this.body.style.fontFamily = 'monospace';
     }
 
     write(text: string): this {
-        this.body.innerText += text;
+        this.state.code += text;
         return this;
     }
 
     overwrite(text: string): this {
-        this.body.innerText = text;
+        this.state.code = text;
         return this;
     }
 }
